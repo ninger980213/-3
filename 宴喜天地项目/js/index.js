baab3bd667063d3d5455142f1a6ff723a6053de6
“X“儿 <1522058821@qq.com>
2020-04-10 19:55:43 +0800

    宴席项目

---
+++ 宴喜天地项目/js/index.js
@@ -0,0 +1,27 @@
+
+$(".circle").click(function(){
+ 
+    $(".ulList").toggle();
+    for (var i = 0; i <$(".ulList li").length; i++) {//lg = 6
+           (function(i){//自执行函数，获取i
+               setTimeout(function() {
+                   console.log($(".ulList li:eq(i)"))
+                   console.log(i)
+                $(".ulList li").eq(i).show();
+               }, i*200)//将 i 的值传进来 ，这样就可以每个一秒输出一个值
+           })(i)
+           
+       }
+      
+
+    
+})
+$(function(){
+
+$(".ulList li").hide();
+
+clearTimeout();
+
+
+
+})
